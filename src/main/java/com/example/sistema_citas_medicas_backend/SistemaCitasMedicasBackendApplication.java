e7a33857a28e782b2a2c8a32acc2304967951afb
dnunezf <david.a.nunez27@outlook.com>
2025-05-12 14:50:33 -0600

    Initial commit

---
+++ src/main/java/com/example/sistema_citas_medicas_backend/SistemaCitasMedicasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistema_citas_medicas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCitasMedicasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCitasMedicasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistema_citas_medicas_backend/SistemaCitasMedicasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistema_citas_medicas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCitasMedicasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
