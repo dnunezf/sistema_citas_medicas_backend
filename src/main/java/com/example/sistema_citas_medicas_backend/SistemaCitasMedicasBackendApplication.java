f150ff55bd709d49817883785320967e35ec84d4
dnunezf <david.a.nunez27@outlook.com>
2025-05-12 16:59:36 -0600

    Division de carpetas

--- src/main/java/com/example/sistema_citas_medicas_backend/SistemaCitasMedicasBackendApplication.java
+++ src/main/java/com/example/sistema_citas_medicas_backend/SistemaCitasMedicasBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SistemaCitasMedicasBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaCitasMedicasBackendApplication.class, args);
 	}
-
 }
