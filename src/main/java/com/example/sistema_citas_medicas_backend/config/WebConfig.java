8ddca401b67006d1feb9495f0334294e98b21c46
mariaaraya <maria.araya.campos@est.una.ac.cr>
2025-05-23 21:47:20 -0600

    Ya se muestra la foto del médico en su perfil

--- src/main/java/com/example/sistema_citas_medicas_backend/config/WebConfig.java
+++ src/main/java/com/example/sistema_citas_medicas_backend/config/WebConfig.java
@@ -3,10 +3,14 @@ package com.example.sistema_citas_medicas_backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer{
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -18,5 +22,14 @@ public class WebConfig {
                         .allowedHeaders("*");
             }
         };
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        Path uploadDir = Paths.get("uploads/fotos_perfil");
+        String uploadPath = uploadDir.toFile().getAbsolutePath();
+
+        registry.addResourceHandler("/uploads/fotos_perfil/**")
+                .addResourceLocations("file:" + uploadPath + "/");
     }
 }
